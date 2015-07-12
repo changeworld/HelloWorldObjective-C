91364697a31494621b287fc97090f6f889f37460
changeworld <changesworlds@gmail.com>
2015-07-12 19:14:49 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by 竹林崇 on 2015/07/12.
+//  Copyright (c) 2015年 Takashi Takebayashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
